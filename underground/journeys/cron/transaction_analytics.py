65e71cfa973966c4d41f2589e6db33a0a0b0be94
lmonish7108 <lmonish7108@gmail.com>
2023-03-07 23:08:18 +0000

    Minor fix

--- underground/journeys/cron/transaction_analytics.py
+++ underground/journeys/cron/transaction_analytics.py
@@ -12,15 +12,15 @@ class JourneyUpload(CronJobBase):
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'transaction_analytics.journey_upload'    # a unique code
 
-
     def do(self):
         journeys = journey.Journey.all_journey_data(
                 columns=['id', 'origin__name', 'destination__name', 
-                         'journey_type', 'metro_card_id', 'timestamp',
+                         'journey_type', 'metro_card_id',
                          'metro_card__userprofile_id', 'metro_card__userprofile__age']
             )
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        requests.post(url=os.getenv('LAMBDA_URL'), data=json.dumps(list(journeys)), headers=headers)
+        if journeys:
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            requests.post(url=os.getenv('LAMBDA_URL'), data=json.dumps(list(journeys)), headers=headers)
 
 
 class JourneyPaymentUpload(CronJobBase):
@@ -33,5 +33,6 @@ class JourneyPaymentUpload(CronJobBase):
         journey_payments = journey.Journey.all_journey_payment_data(
                 columns=['id', 'journey_id', 'price']
             )
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        requests.post(url=os.getenv('LAMBDA_URL'), data=json.dumps(list(journey_payments)), headers=headers)
+        if journey_payments:
+            headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+            requests.post(url=os.getenv('LAMBDA_URL'), data=json.dumps(list(journey_payments)), headers=headers)
